f5a77f5efec0fbf8d2ded7f67d66cb0c8297ad87
adictonator <adityabhaskarsharma@gmail.com>
2022-07-15 01:21:28 +0530

    working on some modularized js logic

--- raw/js/app.js
+++ raw/js/app.js
@@ -1,3 +1,21 @@
-import ImageLoaded from './ImageUploader'
-console.log('ok working')
-ImageLoaded()
+import {
+	SwitchTweetTheme,
+	TwitterInspiration,
+	UpdateTweetScreen,
+} from './TwitterInspiration'
+
+document
+	.getElementById('tweet-inspiration')
+	?.addEventListener('click', (event) => TwitterInspiration(event))
+
+document
+	.getElementById('tweet-url')
+	?.addEventListener('change', (event) => UpdateTweetScreen(event))
+
+// Fetch initial tweet on page load.
+document.getElementById('tweet-url')?.dispatchEvent(new Event('change'))
+
+// Switch tweet theme.
+document
+	.querySelector('[name=theme]')
+	?.addEventListener('click', (event) => SwitchTweetTheme(event))
