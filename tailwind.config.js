bdae7fee9ed7fb804de5d61169c064a484e7bfef
adictonator <adityabhaskarsharma@gmail.com>
2022-07-15 01:20:07 +0530

    updated tailwind config file added variants for child elems

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-	content: ['./assets/*.css', './sections/*.liquid', './templates/*.liquid'],
+	content: ['./sections/*.liquid', './templates/*.liquid'],
 	theme: {
 		extend: {
+			backgroundImage: {
+				tick: 'url(/assets/tick.svg)',
+			},
 			colors: {
 				'brand-red': '#D5003A',
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addVariant }) {
+			addVariant('child', '& :first-child')
+			addVariant('children', '& > *')
+		},
+	],
 }
