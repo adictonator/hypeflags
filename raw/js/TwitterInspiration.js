03df273098af11b87e56e6b161ab529bbd6c82d4
adictonator <adityabhaskarsharma@gmail.com>
2022-07-17 22:17:28 +0530

    some small tweaks code cleanup

--- raw/js/TwitterInspiration.js
+++ raw/js/TwitterInspiration.js
@@ -1,3 +1,5 @@
+import html2canvas from 'html2canvas'
+
 export function TwitterInspiration(event) {
 	event.preventDefault()
 
@@ -63,6 +65,7 @@ export function UpdateTweetScreen(event) {
 			}
 
 			updateTweetContent(data)
+			generateTweetCanvas()
 		})
 		.catch((jqXHR, exception) => {
 			//_parent.addClass('error')
@@ -96,6 +99,8 @@ function updateTweetContent(tweetData) {
 	const time = formatAMPM(data.created_at)
 	const date = formateDate(data.created_at)
 
+	//document.querySelector('button[name="add"]').removeAttribute('disabled')
+
 	$('#custom-loader-buffer').fadeOut('slow', function () {
 		$(this).remove()
 	})
@@ -121,23 +126,9 @@ function updateTweetContent(tweetData) {
 function tweetTemplate(themeClass, design) {
 	let tweet = htmlTweet
 	let html = ''
-	html += '<div class="tweet-flag-wrap"><div class="tweet_flag_wrap_inner">'
-	html += '<div class="upper_dots" data-html2canvas-ignore></div>'
-	html += '<div class="bottom_dots" data-html2canvas-ignore></div>'
-	html += '<div class="tweet_flag_box ' + themeClass + '" id="flagBox">'
-	html += '<div class="tweet_flag_box_inner">'
-	html += '<div class="twitter_wrapper ">'
-	html += '<div class="twitt_head">'
-	html +=
-		'<div class="twitt_u_img"><img src="' +
-		tweet.user.profile_image_url_https +
-		'" alt=""></div>'
-	html += '<div class="twitt_u_info">'
-	html += '<strong>' + tweet.user.name
 
 	if (tweet.user.verified) {
-		html +=
-			'<svg viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5s-2.816.917-3.437 2.25c-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z"></path></g></svg>'
+		html += ''
 	}
 	if (tweet.text) {
 		let text = tweet.text
@@ -270,6 +261,8 @@ export function SwitchTweetTheme(event) {
 			.querySelector('[data-twitter-flag]')
 			?.parentElement?.classList.remove('dark')
 	}
+
+	generateTweetCanvas()
 }
 
 export function showTweetEditor(event) {
@@ -396,4 +389,29 @@ function nFormatter(num, digits = 1) {
 	return item
 		? (num / item.value).toFixed(digits).replace(rx, '$1') + item.symbol
 		: '0'
+}
+
+function generateTweetCanvas() {
+	var fix_screen = 530
+	var fix_scale = 5.7
+	var new_width = $('[data-twitter-flag]').width()
+	var cal_width = new_width / fix_screen
+	var dd = fix_scale / cal_width
+	let element = document.querySelector('[data-twitter-flag]')
+
+	html2canvas(element, {
+		scale: dd,
+		useCORS: true,
+	}).then((canvas) => {
+		const f = canvas.toDataURL('image/png')
+
+		if (document.querySelector('.new_url')) {
+			document.querySelector('.new_url').value = f
+		} else {
+			$('body').append(
+				'<input type="hidden" class="new_url" value="' + f + '"/>'
+			)
+		}
+		$("[name='add']").removeAttr('disabled')
+	})
 }
